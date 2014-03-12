61128815107b7e0843e08197303af802074d243a
MrWaffle <mrwafflewaffle@aim.com>
2014-03-12 22:08:49 +0100

    Rename

--- public/js/loader.js
+++ public/js/loader.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$(window).on('action:ajaxify.end', function(e, data) {
 		if (data.url === "" || data.url === "shoutbox") {
 			requirejs([
-				'plugins/nodebb-plugin-shoutbox/public/js/shoutbox.js'
+				'plugins/nodebb-plugin-shoutbox/public/js/lib/shoutbox.js'
 			], function(shoutBox) {
 				shoutBox.base.init(data.url);
 				if (data.url === "shoutbox") {

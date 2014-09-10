f1ed54615657c572e9f1fed884b43c8662390bee
Schamper <erikschamper@outlook.com>
2014-09-11 00:05:40 +0200

    Fix the 2 admin buttons

--- public/js/admin.js
+++ public/js/admin.js
@@ -105,7 +105,7 @@
 		$('#shoutbox-remove-deleted-button').off('click').on('click', function(e) {
 			bootbox.confirm('Are you sure you wish to remove all shouts marked as deleted from the database?', function(confirm) {
 				if (confirm) {
-					socket.emit('modules.shoutbox.removeAll', {'which':'deleted'}, function(err, result) {
+					socket.emit('plugins.shoutbox.removeAll', {'which':'deleted'}, function(err, result) {
 						if(err) {
 							return app.alertError(err.message);
 						} else {
@@ -119,7 +119,7 @@
 		$('#shoutbox-remove-all-button').off('click').on('click', function(e) {
 			bootbox.confirm('Are you sure you wish to remove all shouts from the database?', function(confirm) {
 				if (confirm) {
-					socket.emit('modules.shoutbox.removeAll', {'which':'all'}, function(err, result) {
+					socket.emit('plugins.shoutbox.removeAll', {'which':'all'}, function(err, result) {
 						if(err) {
 							return app.alertError(err.message);
 						} else {

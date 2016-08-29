e079f3e8d280355ed282e87a10ce5e997b87a618
Schamper <mrwafflewaffle@aim.com>
2016-08-30 01:06:16 +0200

    Fix #87

--- public/js/lib/actions/bug.js
+++ public/js/lib/actions/bug.js
@@ -3,7 +3,7 @@
 (function(Shoutbox) {
 	var Bug = function(sbInstance) {
 		this.register = function() {
-			sbInstance.dom.container.find('#shoutbox-button-bug').off('click').on('click', function() {
+			sbInstance.dom.container.find('.shoutbox-button-bug').off('click').on('click', function() {
 				window.open('https://github.com/Schamper/nodebb-plugin-shoutbox/issues/new', '_blank').focus();
 			});
 		};

9a13e5e393ef261f62f078ca73f6d464ac473917
Jordan Dalton <jordan.8474@gmail.com>
2017-03-13 02:14:23 +0000

    Update gist.js for translations
    Since gist.js loads it's modal via ajaxify, you need to run it through the translator module to translate it.

--- public/js/lib/actions/gist.js
+++ public/js/lib/actions/gist.js
@@ -5,16 +5,22 @@
 	var Gist = function(sbInstance) {
 		this.register = function() {
 			ajaxify.loadTemplate('shoutbox/features/gist', function(tpl){
-				$(document.body).append(tpl);
+				
+				require(['translator'], function(translator) {
+					translator.translate(tpl, function (html) {
+						$(document.body).append(html);
+						
+						var gistModal = $('#shoutbox-modal-gist');
 
-				var gistModal = $('#shoutbox-modal-gist');
+						sbInstance.dom.container.find('.shoutbox-button-gist').off('click').on('click', function(e) {
+							gistModal.modal('show');
+						});
 
-				sbInstance.dom.container.find('.shoutbox-button-gist').off('click').on('click', function(e) {
-					gistModal.modal('show');
-				});
-
-				gistModal.find('#shoutbox-button-create-gist-submit').off('click').on('click', function(e) {
-					createGist(gistModal.find('textarea').val(), gistModal);
+						gistModal.find('#shoutbox-button-create-gist-submit').off('click').on('click', function(e) {
+							createGist(gistModal.find('textarea').val(), gistModal);
+						});
+						
+					});
 				});
 			});
 		};
